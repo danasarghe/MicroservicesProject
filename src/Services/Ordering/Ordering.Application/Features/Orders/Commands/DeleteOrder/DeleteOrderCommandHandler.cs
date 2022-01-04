6dce2d35893da6f30468958d78967ae36a3595f3
INTEGRISOFT\dana.sarghe <dana.sarghe@integrisoft.ro>
2022-01-04 17:37:22 +0200

    Ordering.AplicationLayer - Contracts, Feature(CQRS - Command, Query), Behaviour(ValidationBehaviour), ServiceRegistration

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persitence;
+using Ordering.Application.Exception;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+
+            if (orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            if (orderToDelete != null) _logger.LogInformation($"Order {orderToDelete.Id} is successfully updated");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Querry/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Ordering.Application.Features.Orders.Querry.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+
+        public string UserName { get; set; }
+    }
+}
